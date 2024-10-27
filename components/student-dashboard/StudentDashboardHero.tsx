dc834edfa2f1614ea833a9054548a9e7f11b8ab9
Femi Gabriel <timfemi@gmail.com>
2024-10-27 20:24:49 +0100

    student dashbaord & contentx fixes

--- components/student-dashboard/StudentDashboardHero.tsx
+++ components/student-dashboard/StudentDashboardHero.tsx
@@ -21,7 +21,7 @@ export const StudentDashboardHero = () => {
           <div className="cursor-pointer">
             <Image
               src="/images/Button-1.svg"
-              alt="about-us"
+              alt="resources"
               width={191}
               height={50}
               className="max-w-[191px]"
@@ -34,7 +34,7 @@ export const StudentDashboardHero = () => {
           <div className="cursor-pointer">
             <Image
               src="/images/Button-2.svg"
-              alt="about-us"
+              alt="SEL skill building"
               width={50}
               height={50}
               className="w-full max-w-[191px]"
@@ -42,66 +42,23 @@ export const StudentDashboardHero = () => {
             />
           </div>
         </Link>
-
-
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h3 className="text-[#303030] text-5xl">Welcome to your dashboard</h3>
+        <Link href="/">
+          <p className="text-[#4A4A4A] text-xl mt-3 cursor-pointer">
+            Let's begin.
+          </p>
+        </Link>
       </div>
 
       {/* Footer Navigation */}
-      <div className="flex justify-center items-center gap-5 w-full mb-7">
+      <div className="flex justify-center items-center gap-5 w-full my-5">
         <Link href="/">
           <div className="cursor-pointer">
             <Image
-              src="/images/home-page.svg"
-              alt="about-us"
-              width={191}
-              height={50}
-              className="max-w-[191px]"
-              draggable="false"
-            />
-          </div>
-        </Link>
-        <Link href="/about-us">
-          <div className="cursor-pointer">
-            <Image
-              src="/images/Group 1000007856.svg"
-              alt="about-us"
-              width={50}
-              height={50}
-              className="w-full max-w-[191px]"
-              draggable="false"
-            />
-          </div>
-        </Link>
-        <Link href="/teacher-chat">
-          <div className="cursor-pointer">
-            <Image
-              src="/images/Teacher Chat Button.svg"
-              alt="Teacher Chat"
-              width={191}
-              height={50}
-              className="max-w-[191px]"
-              draggable="false"
-            />
-          </div>
-        </Link>
-        <Link href="/sel-lit">
-          <div className="cursor-pointer">
-            <Image
-              src="/images/SEL and Literature Button.svg"
-              alt="SEL and Literature"
-              width={191}
-              height={50}
-              className="max-w-[191px]"
-              draggable="false"
-            />
-          </div>
-        </Link>
-
-        <Link href="/contact-us">
-          <div className="cursor-pointer">
-            <Image
-              src="/images/Contact Us Button.svg"
-              alt="Contact Us"
+              src="/images/button-3.svg"
+              alt="SEL games"
               width={191}
               height={50}
               className="max-w-[191px]"
