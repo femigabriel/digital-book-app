ff80aaa3eed65d1ace14b2542a2cc16aa72223b1
Femi Gabriel <timfemi@gmail.com>
2024-11-14 14:32:20 +0100

    fixes

--- components/read-book/PageContent.tsx
+++ components/read-book/PageContent.tsx
@@ -60,7 +60,7 @@ export const PageContent = (props: Props) => {
   };
 
   return (
-    <div className="bg-[#F8F8F8] w-full h-full">
+    <div className="bg-[#FFFEE9] w-full h-full">
       <div className="w-full px-5 mb-5 lg:px-10 py-7 text-md flex items-center justify-center leading-8 bg-[#e1d1f]">
         <div className="text-center flex justify-center items-center text-[1em] text-[#18181B] px-10 lg:px-0">
           <p>{props.content}</p>
