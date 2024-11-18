b26b1209a050eb75716559dd9b5f26f460e35afb
Femi Gabriel <timfemi@gmail.com>
2024-11-18 17:28:38 +0100

    fixes

--- components/teacher/book/PageContent.tsx
+++ components/teacher/book/PageContent.tsx
@@ -62,7 +62,7 @@ export const PageContent = (props: Props) => {
   return (
     <div className="bg-[#FFFEE9] shadow w-full h-full">
       <div className="w-full px-5 mb-5 lg:px-10 py-7 text-md flex items-center justify-center leading-8 bg-[#e1d1f]">
-        <div className="text-center flex justify-center items-center text-sm text-[#18181B] px-5 lg:px-0">
+        <div className="text-center flex justify-center items-center  mt-7 text-sm text-[#18181B] px-5 lg:px-0">
           <p>{props.content}</p>
         </div>
       </div>
